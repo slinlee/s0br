cde826887c85951f8d8fa431f38d33988b67f405
Slin Lee <slinlee@gmail.com>
2022-03-04 16:02:39 -0800

    Add wallet component to connect to metamask

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Wallet from '../components/Wallet.svelte';
+</script>
+
+<h1>Welcome to S0BR</h1>
+
+<Wallet />
