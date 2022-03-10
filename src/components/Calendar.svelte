a461699b303319500607872e41c413d3d93f7a81
Slin Lee <slinlee@gmail.com>
2022-03-10 11:10:55 -0800

    Add placeholder calendar component

---
+++ src/components/Calendar.svelte
@@ -0,0 +1,1 @@
+<h1>Calendar Compenent</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
+  import Calendar from "../components/Calendar.svelte";
   import Wallet from "../components/Wallet.svelte";
 </script>
 
 <h1>Welcome to S0BR</h1>
 
 <Wallet />
+<Calendar />
