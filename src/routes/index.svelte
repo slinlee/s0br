9d776950a6953ba639dafd4e3c5f212091a25996
Slin Lee <slinlee@gmail.com>
2022-03-10 12:46:29 -0800

    Add placeholder main CTA button (#16)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
   import Calendar from "../components/Calendar.svelte";
   import Wallet from "../components/Wallet.svelte";
+  import CommitmentButton from "../components/Commitment-Button.svelte";
 </script>
 
 <h1>Welcome to S0BR</h1>
 
 <Wallet />
 <Calendar />
+<CommitmentButton />
