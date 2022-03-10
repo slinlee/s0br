0faeb96f323d2c9c3b03f8749b74604db8aaa490
Slin Lee <slinlee@gmail.com>
2022-03-10 14:59:42 -0800

    Style up the main commitment button

--- src/components/Commitment-Button.svelte
+++ src/components/Commitment-Button.svelte
@@ -1,3 +1,5 @@
-<div class="flex justify-center">
-  <button>I am not drinking today</button>
-</div>
+<button
+  class="rounded p-6 shadow-lg hover:shadow-2xl transition bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 inline-flex items-center"
+>
+  I am not drinking today
+</button>
