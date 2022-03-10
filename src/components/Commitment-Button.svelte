bf61cd4ddc1e7d08b6b59aea5fb888b30dd9e5d1
Slin Lee <slinlee@gmail.com>
2022-03-10 15:23:48 -0800

    Tinker with button style again

--- src/components/Commitment-Button.svelte
+++ src/components/Commitment-Button.svelte
@@ -1,5 +1,18 @@
 <button
-  class="rounded p-6 shadow-lg hover:shadow-2xl transition bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 inline-flex items-center"
+  class="rounded 
+    p-6 
+    shadow-lg 
+    hover:shadow-2xl 
+    transition 
+    duration-75
+    bg-gray-300 
+    active:shadow
+    text-gray-800 
+    font-bold 
+    py-2 
+    px-4 
+    inline-flex 
+    items-center"
 >
   I am not drinking today
 </button>
