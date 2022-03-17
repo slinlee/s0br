635fe0424ed0e38600a3c56b3c430a78c245e9c0
Slin Lee <slinlee@gmail.com>
2022-03-16 21:27:17 -0700

    Remove todo note

--- src/routes/api/nft/[account].ts
+++ src/routes/api/nft/[account].ts
@@ -1,13 +1,16 @@
 /** @type {import('./[account]').RequestHandler} */
 
 export async function get({ params }) {
-  const apiKey: string = import.meta.env.VITE_MORALIS_API_KEY; // TODO get the api key from env var
+  const apiKey: string = import.meta.env.VITE_MORALIS_API_KEY;
   const tokenContract = `0x2a0493dee4f4b5e4b595326f0e73645f6f493923`;
   const url = `https://deep-index.moralis.io/api/v2/${params?.account}/nft/${tokenContract}?chain=polygon&format=decimal`;
 
   const res = await fetch(url, {
     method: `GET`,
-    headers: { accept: "application/json", "X-API-Key": apiKey },
+    headers: {
+      accept: "application/json",
+      "X-API-Key": apiKey,
+    },
   });
   let response = await res.json();
   response = response.result;
