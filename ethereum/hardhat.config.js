d6fe3dc63d308875de95fcd2e4917790a59b2523
Slin Lee <slinlee@gmail.com>
2022-03-24 16:54:02 -0700

    Add hardhat config for mumbai testnet

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,8 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.MUMBAI_PRIVATE_KEY]
+    }
+  },
 };
