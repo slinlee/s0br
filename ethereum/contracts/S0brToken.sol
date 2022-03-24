4711a77aed7c7ea1d8ecccf7aa9cd6da29f33cbe
Slin Lee <slinlee@gmail.com>
2022-03-24 16:52:40 -0700

    Make S0brToken burnable

--- ethereum/contracts/S0brToken.sol
+++ ethereum/contracts/S0brToken.sol
@@ -1,9 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0 <=0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract S0brToken is ERC20 {
+contract S0brToken is ERC20, ERC20Burnable {
 
     uint constant _initial_supply = 10_000_000 * (10**18);
 
