d6d77453957bc54f83f348763f5819ad851fcd50
Slin Lee <slin@slinlee.com>
2022-04-06 12:06:27 -0700

    Set up the deploy game script

--- ethereum/scripts/deploy.js
+++ ethereum/scripts/deploy.js
@@ -1,9 +1,19 @@
 const { ethers, upgrades } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
+  const timeout = 1440;
+  const faucetDripBase = 1;
+  const faucetDripDecimal = 18;
+
   const Game = await ethers.getContractFactory("S0brGame");
   console.log("Deploying Game...");
-  const game = await upgrades.deployProxy(Game); // TODO check this line
+  const game = await upgrades.deployProxy(Game, [
+    process.env.MUMBAI_TOKEN_ADDRESS,
+    faucetDripBase,
+    faucetDripDecimal,
+    timeout,
+  ]); // TODO check this line
   await game.deployed();
   console.log("Game deployed to: ", game.address);
 }
