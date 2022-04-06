2fdf0c68eb9b080b9af1cff2a863946554c2e1fb
Slin Lee <slin@slinlee.com>
2022-04-06 14:02:58 -0700

    Add basics to get the users balance of s0br tokens

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,13 @@
   import Footer from "../components/Footer.svelte";
   import Wallet from "../components/Wallet.svelte";
   import CommitmentButton from "../components/Commitment-Button.svelte";
+  import S0brToken from "../routes/contracts/S0brToken.sol/S0brToken.json";
+
+  const Token = S0brToken;
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-12">
-  <Wallet />
+  <Wallet tokenContract={Token} />
   <h1 class="col-span-12 text-3xl py-20 place-self-center">Welcome to S0BR</h1>
 
   <Calendar />
