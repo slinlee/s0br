f8b905c62e82a1e12ac5461d6829b9cff0eb7021
Slin Lee <slinlee@gmail.com>
2022-04-06 23:18:40 -0700

    Hook up calendar data (#51)
    * Update lockfile
    
    * wip - get real data and show it in the heatmap
    
    * WIP - not working
    
    * wip - calendar shows again with no data
    
    * Got basics to work
    
    Need to manually load the data
    
    * Move load button

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,31 +1,39 @@
-<script>
-  import { add, sub, isBefore } from "date-fns";
+<script lang="ts">
+  import { toDate, add, sub, isBefore } from "date-fns";
   import SvelteHeatmap from "svelte-heatmap";
 
-  // Generate a random number
-  function rand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  import { ethers } from "ethers";
+  import S0brGame from "../routes/contracts/S0brGame.sol/S0brGame.json";
 
-  // Generate dummy data for a heatmap
-  function generateFakeData() {
-    const data = [];
-    const now = new Date();
-    let date = sub(now, { years: 1 });
+  let cleanedData = [];
 
-    while (isBefore(date, now)) {
-      data.push({
-        date,
-        value: rand(0, 1),
+  async function getCommitments() {
+    const gameAddress = "0xD4Cc23e2fBE876ef5122bA97a4e8840D92CA3a81";
+    const gameContract = S0brGame;
+
+    let data = [];
+
+    if (typeof window?.ethereum !== "undefined") {
+      const [account] = await window.ethereum.request({
+        method: "eth_requestAccounts",
       });
-
-      date = add(date, { days: 1 });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        gameAddress,
+        gameContract.abi,
+        signer
+      );
+      data = await contract.getCommitments(account);
+      data.forEach((item) => {
+        cleanedData = [
+          ...cleanedData,
+          { date: toDate(item.toNumber() * 1000), value: 1 },
+        ];
+      });
     }
-
-    return data;
+    return cleanedData;
   }
-
-  const fakeData = generateFakeData();
 </script>
 
 <div
@@ -33,17 +41,19 @@
   data-test="calendar"
 >
   <SvelteHeatmap
-    allowOverflow={true}
+    allowOverflow={false}
     cellGap={5}
     cellRadius={1}
+    endDate={new Date()}
     colors={["#a1dab4", "#42b6c4", "#2c7fb9", "#263494"]}
-    data={fakeData}
+    data={cleanedData}
     dayLabelWidth={0}
     emptyColor={"#ecedf0"}
-    endDate={new Date()}
     monthGap={20}
     monthLabelHeight={0}
-    startDate={sub(new Date(), { weeks: 1 })}
+    startDate={sub(new Date(), { months: 1 })}
     view={"monthly"}
   />
 </div>
+
+<div on:click={getCommitments} class="col-start-4">load</div>
