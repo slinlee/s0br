1d691101d630a32b36dea25fcbdb8def5a8b9924
Slin Lee <slin@slinlee.com>
2022-04-06 22:11:24 -0700

    wip - get real data and show it in the heatmap

--- src/components/Calendar.svelte
+++ src/components/Calendar.svelte
@@ -1,6 +1,43 @@
-<script>
+<script lang="ts">
   import { add, sub, isBefore } from "date-fns";
   import SvelteHeatmap from "svelte-heatmap";
+
+  import { BigNumber, ethers } from "ethers";
+  import S0brGame from "../routes/contracts/S0brGame.sol/S0brGame.json";
+
+  let cleanedData = [];
+
+  async function getCommitments() {
+    const gameAddress = "0xD4Cc23e2fBE876ef5122bA97a4e8840D92CA3a81";
+    const gameContract = S0brGame;
+
+    let data = [];
+
+    if (typeof window?.ethereum !== "undefined") {
+      const [account] = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        gameAddress,
+        gameContract.abi,
+        signer
+      );
+      data = await contract.getCommitments(account);
+      data.forEach(
+        (item) =>
+          (cleanedData = [
+            ...cleanedData,
+            { date: new Date(item.toNumber() * 1000), value: 1 },
+          ])
+      );
+      console.table(cleanedData); // debug
+    }
+    return cleanedData;
+  }
+
+  // const data = getCommitments();
 
   // Generate a random number
   function rand(min, max) {
@@ -26,8 +63,12 @@
   }
 
   const fakeData = generateFakeData();
+  // console.table(fakeData); // debug
 </script>
 
+<div on:click={getCommitments}>load</div>
+
+<div>{cleanedData}</div>
 <div
   class="px-6 md:col-start-3 md:col-end-11 lg:col-start-4 lg:col-end-10"
   data-test="calendar"
@@ -37,13 +78,12 @@
     cellGap={5}
     cellRadius={1}
     colors={["#a1dab4", "#42b6c4", "#2c7fb9", "#263494"]}
-    data={fakeData}
+    data={cleanedData}
     dayLabelWidth={0}
     emptyColor={"#ecedf0"}
-    endDate={new Date()}
     monthGap={20}
     monthLabelHeight={0}
-    startDate={sub(new Date(), { weeks: 1 })}
+    startDate={sub(new Date(), { years: 1 })}
     view={"monthly"}
   />
 </div>
