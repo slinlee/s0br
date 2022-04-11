0e1c4902055489d2ed11f60c34051c31b7030528
Slin Lee <slin@slinlee.com>
2022-04-11 11:48:36 -0700

    Add balance to store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -4,3 +4,4 @@ export const walletConnected = writable(false);
 export const account = writable();
 export const network = writable("");
 export const commitments = writable([]);
+export const balance = writable();
