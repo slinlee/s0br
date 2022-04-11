91018441ff10bfac62ec05aa2229949a4baffd1a
Slin Lee <slin@slinlee.com>
2022-04-11 11:29:41 -0700

    Add initial store

---
+++ src/lib/stores.js
@@ -0,0 +1,6 @@
+import { writable } from "svelte/store";
+
+export const walletConnected = writable(false);
+export const account = writable();
+export const network = writable("");
+export const commitments = writable([]);
