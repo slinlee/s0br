8f2948dd89b32ae42974524e34e32d9b948012a8
Slin Lee <slin@slinlee.com>
2022-04-13 12:12:09 -0700

    Refactor functions to get data to shared lib

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,9 @@
 <script>
-  import { toDate, add, sub, isBefore } from "date-fns";
-
+  import { browser } from "$app/env";
   import Calendar from "../components/Calendar.svelte";
   import Footer from "../components/Footer.svelte";
   import Wallet from "../components/Wallet.svelte";
   import CommitmentButton from "../components/Commitment-Button.svelte";
-
-  import S0brToken from "../routes/contracts/S0brToken.sol/S0brToken.json";
-  import S0brGame from "../routes/contracts/S0brGame.sol/S0brGame.json";
-
-  import { browser } from "$app/env";
-  import { ethers } from "ethers";
 
   import {
     balance,
@@ -20,60 +13,18 @@
     commitments,
   } from "$lib/stores.js";
 
-  const gameAddress = "0x28D4aAc8Dc916bAd9778313df9334334A7e04A6A";
-  const tokenAddress = "0x139159c21171aB09c46A027503aFD6b91E3A0851";
+  import { getData } from "$lib/s0br.js";
 
-  async function setup() {
-    if (browser && typeof window.ethereum !== "undefined") {
-      const [firstAccount] = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      account.set(firstAccount);
-
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const gameContract = new ethers.Contract(
-        gameAddress,
-        S0brGame.abi,
-        signer
-      );
-      const tokenContract = new ethers.Contract(
-        tokenAddress,
-        S0brToken.abi,
-        signer
-      );
-      walletConnected.set(true);
-
-      // Get balance
-      let bal = await tokenContract.balanceOf($account);
-      balance.set(ethers.utils.formatEther(bal));
-
-      // Get network
-      let net = await provider.getNetwork();
-      network.set(net.name);
-
-      // Get calendar data
-
-      let data = [];
-      let cleanedData = [];
-
-      data = await gameContract.getCommitments($account);
-      data.forEach((item) => {
-        cleanedData = [
-          ...cleanedData,
-          { date: toDate(item.toNumber() * 1000), value: 1 },
-        ];
-      });
-
-      commitments.set(cleanedData);
-    } else {
-      // Show user 'Connect to Metamask'
-    }
+  if (browser && $walletConnected) {
+    getData();
   }
 
-  if (browser) {
-    setup();
-  }
+  // account.subscribe(()=> {console.log("getting data - account changed");
+  // getData();
+  // })
+  // network.subscribe(()=> {console.log("getting data - network changed");
+  // getData();
+  // })
 </script>
 
 <head>
