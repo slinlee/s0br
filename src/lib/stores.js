64d02d87d8be1c1291f013a290cf847da6007239
Slin Lee <slin@slinlee.com>
2022-04-17 16:22:50 -0700

    Add error message to store

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -5,3 +5,4 @@ export const account = writable();
 export const network = writable("");
 export const commitments = writable([]);
 export const balance = writable();
+export const errorMsg = writable();
