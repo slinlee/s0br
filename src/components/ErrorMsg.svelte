0a9cb221a495e5617a832fb13aba4c27d87f69bd
Slin Lee <slin@slinlee.com>
2022-04-17 17:42:42 -0700

    Add Error Message component

---
+++ src/components/ErrorMsg.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { errorMsg } from "$lib/stores.js";
+</script>
+
+{#if $errorMsg}
+  <div
+    class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+    role="alert"
+  >
+    {$errorMsg}
+  </div>
+{/if}
