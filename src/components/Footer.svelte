8e27b7197f89ffde392dcf56350d9195d67a2e45
Slin Lee <slinlee@gmail.com>
2022-04-25 19:31:24 -0700

    Rename `Debugging Stats` to just `Stats` (#108)

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -10,7 +10,7 @@
 
 <div class="col-span-12 place-self-end m-8 mt-20">
   <a href="https://discord.gg/y7PGJRZvPa" target="_blank">Discord</a>
-  - <span on:click={toggleStats}>Debugging Stats</span>
+  - <span on:click={toggleStats}>Stats</span>
 </div>
 
 {#if showStats}
