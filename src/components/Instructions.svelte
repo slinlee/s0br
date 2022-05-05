cb19aafe48230ec5c8a3cace1719369d797c7de1
Slin Lee <slinlee@gmail.com>
2022-05-05 16:27:36 -0700

    Add instructions (#90)
    * Add instructions
    
    * Rename `Debugging Stats` to just `Stats`
    
    * Wip - add instructions content
    
    * Wip - add more basic instructions
    
    * Remove unused import
    
    * Refactor addToken()
    
    * Add Instructions content
    
    * Add basic prose styles for Tailwind
    
    * Add link for connecting metamask
    
    * Add step for making first commitment

---
+++ src/components/Instructions.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { network, walletConnected, commitments } from "$lib/stores.js";
+  import { addToken, getData } from "$lib/s0br.js";
+</script>
+
+<div
+  class="px-6 py-8  md:col-start-3 md:col-end-11 lg:col-start-4 lg:col-end-10"
+>
+  <div class="prose py-4">
+    <h2 class="font-bold font-lg">What is S0BR?</h2>
+
+    <p>It's a commitment to yourself.</p>
+    <p>
+      Each morning, you make a decision, "I am not drinking today". And just by
+      committing to that in the morning, it's easier the rest of the day when
+      you get to make a choice. A friend offers you a beer at a picnic, "No
+      thanks, I'm not drinking today." Somehow it's easier if you're already
+      made the decision.
+    </p>
+    <p>
+      S0BR helps you track each day and displays it in a beautiful calendar that
+      visualizes your progress and how each day builds into something much
+      bigger.
+    </p>
+    <p>
+      Along the way, each day you use S0BR, you'll get a S0BR Token, and at
+      personal milestones you'll be rewarded with S0BR NFT.
+    </p>
+    <p>So come back to S0BR each day and choose.</p>
+  </div>
+
+  <div class="prose py-4">
+    <h2 class="font-bold font-lg">Get started</h2>
+    <ol class="list-outside">
+      <li>
+        {#if $walletConnected}☑️{:else}🔲{/if}
+        <a href="javascript:void(0)" on:click={getData}>Connect Metamask</a>
+      </li>
+      <li>
+        {#if $network === "matic"}☑️{:else}🔲{/if} Switch to Polygon Network
+      </li>
+      <li>
+        🔲 <a href="javascript:void(0)" on:click={addToken}
+          >Add S0BR Token to Metamask.</a
+        > (We can't tell if you've done this yet)
+      </li>
+      <li>
+        {#if $commitments.length > 0}
+          ☑️{:else}🔲{/if} Commit to your first day of S0BR
+      </li>
+    </ol>
+  </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,8 @@
   import Footer from "../components/Footer.svelte";
   import Wallet from "../components/Wallet.svelte";
   import CommitmentButton from "../components/Commitment-Button.svelte";
+  import Instructions from "../components/Instructions.svelte";
+
   import ErrorMsg from "../components/ErrorMsg.svelte";
 
   import { getData } from "$lib/s0br.js";
@@ -38,6 +40,8 @@
   <div class="place-self-center col-span-12">
     <CommitmentButton />
   </div>
+
+  <Instructions />
 
   <Footer />
 </div>
